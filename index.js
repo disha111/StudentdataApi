4c3e58a3e51a657d337917b260b88fb400e42969
Rahul Jagetiya <rjagetiya780@rku.ac.in>
2021-04-22 23:28:03 +0530

    Bug for heroku

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const bodyParser = require('body-parser');
 const route = require('./route');
 const cors = require('cors');
 
-//connect mongoDB
-mongoose.connect("mongodb+srv://DJ:Disha123@cluster0.ogawy.mongodb.net/students?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(
-    ()=>{
+//connect to mongodb
+mongoose.connect("mongodb+srv://DJ:Disha123@cluster0.ogawy.mongodb.net/students?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true }).then(
+    () => {
         const app = express();
         app.use(cors());
-        app.use(bodyParser.urlencoded({extended:true}));
+        app.use(bodyParser.urlencoded({ extended: true }));
         app.use(express.json());
-        app.use("/api",route);
-        app.listen(process.env.PORT || 3000,()=>{
-            console.log("Server started...!");
-        })
+        app.use("/api", route);
+        app.listen(process.env.PORT || 3000, () => {
+            console.log('Server Got Started....!!');
+        });
     }
-)
+);
