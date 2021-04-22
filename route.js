2179787366923f6cabbd56423e81f2aa1ead87c7
Rahul Jagetiya <rjagetiya780@rku.ac.in>
2021-04-22 23:45:35 +0530

    Bugfixes for heroku

--- route.js
+++ route.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const StudentData = require("./Model/studentdata");
 const Student = require("./Model/student");
 const bookdata = require("./Model/book");
 const Video = require("./Model/video");
@@ -9,89 +8,84 @@ const auth = require("./verifyToken");
 
 const router = express.Router();
 
-router.get("/studentData", async (req, res) => {
-  const student = await StudentData.find();
-  res.send(student);
-}); 
-
 router.get("/bookdata", async (req, res) => {
   const book = await bookdata.find();
   res.send(book);
 });
 
-router.post("/bookdata", async (req, res) => {
-  const book = new bookdata({
-    name: req.body.name,
-    qty: req.body.qty,
-  });
-  await book.save();
-  res.send(book);
-});
-router.delete("/bookdata/:name", async (req, res) => {
-  await bookdata.deleteOne({ name: req.params.name }, (err, d) => {
-    if (err)
-      return res.status(400).send({ err: "Book is not found!! No such data." });
-    if (d.deletedCount > 0) res.send("Book data is deleted successfully");
-    else res.send("Record doesn't exist or already deleted");
-  });
-});
-router.patch("/bookdata/:name", async (req, res) => {
-  try {
-    const book = await bookdata.findOne({ name: req.params.name });
-    book.name = req.body.name;
-    await book.save();
-    res.send(book);
-  } catch (error) {
-    res.send(error);
-  }
-});
+// router.post("/bookdata", async (req, res) => {
+//   const book = new bookdata({
+//     name: req.body.name,
+//     qty: req.body.qty,
+//   });
+//   await book.save();
+//   res.send(book);
+// });
+// router.delete("/bookdata/:name", async (req, res) => {
+//   await bookdata.deleteOne({ name: req.params.name }, (err, d) => {
+//     if (err)
+//       return res.status(400).send({ err: "Book is not found!! No such data." });
+//     if (d.deletedCount > 0) res.send("Book data is deleted successfully");
+//     else res.send("Record doesn't exist or already deleted");
+//   });
+// });
+// router.patch("/bookdata/:name", async (req, res) => {
+//   try {
+//     const book = await bookdata.findOne({ name: req.params.name });
+//     book.name = req.body.name;
+//     await book.save();
+//     res.send(book);
+//   } catch (error) {
+//     res.send(error);
+//   }
+// });
 
-router.post("/register", async (req, res) => {
-  const salt = await bcrypt.genSalt(10);
-  const hashpwd = await bcrypt.hash(req.body.pwd, salt);
-  const reg = new StudentData({
-    userName: req.body.uname,
-    userPwd: hashpwd,
-  });
-  await reg.save();
-  res.send(reg);
-});
+// router.post("/register", async (req, res) => {
+//   const salt = await bcrypt.genSalt(10);
+//   const hashpwd = await bcrypt.hash(req.body.pwd, salt);
+//   const reg = new StudentData({
+//     userName: req.body.uname,
+//     userPwd: hashpwd,
+//   });
+//   await reg.save();
+//   res.send(reg);
+// });
 
-router.post("/login", async (req, res) => {
-  const user = await StudentData.findOne({ userName: req.body.uname });
-  if (!user) return res.send("Invalid User...");
-  const isvalid = await bcrypt.compare(req.body.pwd, user.userPwd);
-  if (isvalid) {
-    const token = jwt.sign({ _id: user._id }, "privatekey");
-    res.header("auth-token", token);
-    res.send(token);
-  }
-});
+// router.post("/login", async (req, res) => {
+//   const user = await StudentData.findOne({ userName: req.body.uname });
+//   if (!user) return res.send("Invalid User...");
+//   const isvalid = await bcrypt.compare(req.body.pwd, user.userPwd);
+//   if (isvalid) {
+//     const token = jwt.sign({ _id: user._id }, "privatekey");
+//     res.header("auth-token", token);
+//     res.send(token);
+//   }
+// });
 
-router.get("/video", auth, async (req, res) => {
-  const video = await Video.find();
-  res.send(video);
-});
+// router.get("/video", auth, async (req, res) => {
+//   const video = await Video.find();
+//   res.send(video);
+// });
 
-router.post("/video", auth, async (req, res) => {
-  const video = new Video({
-    title: req.body.title,
-    desc: req.body.desc,
-    posted_by: req.body.posted_by,
-    url: req.body.url,
-    likes: req.body.likes,
-    cat: req.body.cat,
-  });
-  await video.save();
-  res.send(video);
-});
+// router.post("/video", auth, async (req, res) => {
+//   const video = new Video({
+//     title: req.body.title,
+//     desc: req.body.desc,
+//     posted_by: req.body.posted_by,
+//     url: req.body.url,
+//     likes: req.body.likes,
+//     cat: req.body.cat,
+//   });
+//   await video.save();
+//   res.send(video);
+// });
 
-router.get("/pvdata", auth, (req, res) => {
-  res.json({
-    title: "No Data...!",
-    desc: "No Data...!",
-  });
-});
+// router.get("/pvdata", auth, (req, res) => {
+//   res.json({
+//     title: "No Data...!",
+//     desc: "No Data...!",
+//   });
+// });
 
 router.delete("/rkuStudent/:id", async (req, res) => {
   await Student.deleteOne({ _id: req.params.id }, (err, d) => {
@@ -141,10 +135,10 @@ router.post("/rkuStudent", async (req, res) => {
      }
    });
    
-   router.get("/rkuStudent", auth, async (req, res) => {
-    const rkuStudent = await Student.find();
-    res.send(rkuStudent);
-  });
+  router.get("/rkuStudent", auth, async (req, res) => {
+  const rkuStudent = await Student.find();
+  res.send(rkuStudent);
+});
 
   router.post("/ulogin", async (req, res) => {
     const user = await Student.findOne({ email: req.body.email });
@@ -157,18 +151,19 @@ router.post("/rkuStudent", async (req, res) => {
     }
   });
 
-  router.get('/user/:id',auth,async(req,res)=>{
-    const user = await StudentData.findOne({_id:req.params.id});
-  });
 
 router.post("/register", async (req, res) => {
   try {
+    const salt = await bcrypt.genSalt(10);
+    const hashpwd = await bcrypt.hash(req.body.pass, salt);
     const rkuStudent = new Student({
       name: req.body.name,
       rno: req.body.rno,
+      email: req.body.email,
+      pass: hashpwd,
       branch: req.body.branch,
       dob: req.body.dob,
-      gen: req.body.gen,
+      gen: req.body.gen1
     });
 
     await rkuStudent.save();
